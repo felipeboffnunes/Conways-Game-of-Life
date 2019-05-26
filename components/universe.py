26621c9eea9de307dfb0db112bc4a3c65eb9550e
Kor <felipeboffnunes@protonmail.com>
2019-05-25 22:07:04 -0300

    gitignore pycache. Init on Components

--- components/universe.py
+++ components/universe.py
@@ -1,6 +1,14 @@
+from .cell import Cell
+
 class Universe:
     def __init__(self, x_size:int, y_size:int):
         self.x_size = x_size
         self.y_size = y_size
         self.seed = [[0]*x_size]*y_size
         
+    def populate(self):
+        cell = Cell(9,9,0)
+        print(cell)
+
+    def iterate(self):
+        state = self.seed
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from components.universe import Universe
+
 def main():
     print("Welcome to Conway's Game of Life, built in Python by Felipe Boff Nunes at 25/05/2019")
-
+    universe = Universe(10,10)
+    universe.populate()
 if __name__ == "__main__":
     main()
