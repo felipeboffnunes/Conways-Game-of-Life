b81af29ff60d2994d3b1ad2c435f6863ef07776e
Kor <felipeboffnunes@protonmail.com>
2019-05-25 23:13:28 -0300

    Looking at neighbours, but need to fix index range

--- components/universe.py
+++ components/universe.py
@@ -1,25 +1,40 @@
 from .cell import Cell
 
+
 class Universe:
-    def __init__(self, x_size:int, y_size:int):
+    def __init__(self, x_size: int, y_size: int):
         self.x_size = x_size
         self.y_size = y_size
         self.seed = [[]*x_size]*y_size
 
     def fate(self, cell, current_universe):
-        coordinates = cell.x, cell.y
-        print(coordinates)
+        neighbours = current_universe[cell.x + 1][cell.y + 1], \
+            current_universe[cell.x + 1][cell.y + 1], \
+            current_universe[cell.x][cell.y + 1], \
+            current_universe[cell.x - 1][cell.y + 1], \
+            current_universe[cell.x + 1][cell.y], \
+            current_universe[cell.x + 1][cell.y - 1], \
+            current_universe[cell.x - 1][cell.y], \
+            current_universe[cell.x - 1][cell.y - 1]
+
+        neighbours_alive = [cell.state for cells in neighbours]
+        print(neighbours_alive)
+
         def rule_1():
-            return False
-        
+            if neighbours_alive > 2:
+                return False
+
         def rule_2():
-            return True
+            if neighbours_alive == 2 or neighbours_alive == 3:
+                return True
 
         def rule_3():
-            return False
+            if neighbours_alive > 3:
+                return False
 
         def rule_4():
-            return True
+            if neighbours_alive == 3:
+                return True
 
         if cell.state:
             cell.state = rule_1() or rule_2() or rule_3()
@@ -27,20 +42,19 @@ class Universe:
             cell.state = rule_4()
         new_state = False
         return new_state
-        
+
     def iterate(self):
         current_universe = self.seed
-        update = lambda cell, current_universe: self.fate(cell, current_universe)
-        self.seed = [[update(current_universe[row][cell], current_universe) \
-                    for cell in range(len(current_universe[row]))] \
-                    for row in range(len(current_universe))]
+        def update(cell, current_universe): return self.fate(
+            cell, current_universe)
+        self.seed = [[update(current_universe[row][cell], current_universe)
+                      for cell in range(self.x_size)]
+                     for row in range(self.y_size)]
         print(self.seed)
 
     def start(self):
-        generate = lambda x, y: Cell(x, y, False)
-        self.seed = [[generate(cell, row) \
-                    for cell in range(self.x_size)] \
-                    for row in range(self.y_size)]
+        def generate(x, y): return Cell(x, y, False)
+        self.seed = [[generate(cell, row)
+                      for cell in range(self.x_size)]
+                     for row in range(self.y_size)]
         print(self.seed)
-        
-
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ def main():
     print("Welcome to Conway's Game of Life, built in Python by Felipe Boff Nunes at 25/05/2019")
     universe = Universe(2,2)
     universe.start()
+    universe.iterate()
 if __name__ == "__main__":
     main()
